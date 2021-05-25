e0c9f6160d5cc5d2d72f07fa066e50fd4aa87f2e
syriner <syrineriahi11@gmail.com>
2021-05-25 16:08:02 +0100

    modification invoice

--- invoice_custom/__manifest__.py
+++ invoice_custom/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','studio_customization'],
+    'depends': ['base','account',],
 
     # always loaded
     'data': [
--- invoice_custom/models/models.py
+++ invoice_custom/models/models.py
@@ -8,21 +8,22 @@ class AccountMove(models.Model):
     _description = "Journal Entry"
 
     # ajout champs amount_market dans le modele facturation
-    amount_market = fields.Monetary(string='Montant Marché', readonly=True, compute='_get_amount_market_sale')
+    amount_market = fields.Monetary(string='Montant Marché', compute='_get_amount_market_sale')
     advance = fields.Boolean(string="% site section", compute='_get_per_advance_categ', default=False)
 
+    # @api.depends('partner_id', 'state')
     def _get_amount_market_sale(self):
         """
         Calculer Montant Marché par client.
         """
         sale_ids = self.env['sale.order'].search([('partner_id', '=', self.partner_id.id), ('state', '=', 'sale')])
-        amount_market = 0.0
+        amount = 0.0
         for par in sale_ids:
-            amount_market += par.amount_total
+            amount += par.amount_total
 
-        self.amount_market = amount_market
+        self.amount_market = amount
 
-    # @api.depends('invoice_line_ids')
+    @api.depends('invoice_line_ids')
     def _get_per_advance_categ(self):
         """
         Calculer advance.
